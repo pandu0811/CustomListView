608351f726d90d82d34048846fde5956c12e115d
pandu0811 <pandu.1999@students.amikom.ac.id>
2020-10-26 09:33:52 +0700

    CustomListView

---
+++ app/src/main/java/com/pcs/pcs_customlistview/Movie.java
@@ -0,0 +1,27 @@
+package com.pcs.pcs_customlistview;
+
+public class Movie {
+    private String title;
+    private String year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
